14a97813ae7626dee0aa77149394496d4fd69e48
ELDACSA <65476764+EA5296793@users.noreply.github.com>
2024-05-18 10:29:08 -0500

    Create fosdb.sql

---
+++ BancoProyectos/GRUPO_6/CodigoBarMoe/database/fosdb.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-05-2022 a las 05:50:43
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `fosdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `ID` int(2) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`ID`, `username`, `password`) VALUES
+(0, 'admin', '1234abcd..');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_menu`
+--
+
+CREATE TABLE `tbl_menu` (
+  `menuID` int(11) NOT NULL,
+  `menuName` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_menu`
+--
+
+INSERT INTO `tbl_menu` (`menuID`, `menuName`) VALUES
+(13, 'licores'),
+(14, 'snaks');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_menuitem`
+--
+
+CREATE TABLE `tbl_menuitem` (
+  `itemID` int(11) NOT NULL,
+  `menuID` int(11) NOT NULL,
+  `menuItemName` text NOT NULL,
+  `price` decimal(15,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_menuitem`
+--
+
+INSERT INTO `tbl_menuitem` (`itemID`, `menuID`, `menuItemName`, `price`) VALUES
+(40, 13, 'Ron', '150000.00'),
+(41, 13, 'tequila', '100000.00'),
+(42, 14, 'papas', '5000.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_order`
+--
+
+CREATE TABLE `tbl_order` (
+  `orderID` int(11) NOT NULL,
+  `status` text NOT NULL,
+  `total` decimal(15,2) NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_order`
+--
+
+INSERT INTO `tbl_order` (`orderID`, `status`, `total`, `order_date`) VALUES
+(1, 'ready', '155000.00', '2022-05-09'),
+(2, 'preparing', '150000.00', '2022-05-09'),
+(3, 'preparing', '100000.00', '2022-05-09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_orderdetail`
+--
+
+CREATE TABLE `tbl_orderdetail` (
+  `orderID` int(11) NOT NULL,
+  `orderDetailID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_orderdetail`
+--
+
+INSERT INTO `tbl_orderdetail` (`orderID`, `orderDetailID`, `itemID`, `quantity`) VALUES
+(1, 17, 40, 1),
+(1, 18, 42, 1),
+(2, 19, 40, 1),
+(3, 20, 41, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_role`
+--
+
+CREATE TABLE `tbl_role` (
+  `role` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_role`
+--
+
+INSERT INTO `tbl_role` (`role`) VALUES
+('Cajero'),
+('Mesero');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_staff`
+--
+
+CREATE TABLE `tbl_staff` (
+  `staffID` int(2) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` text NOT NULL,
+  `role` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_staff`
+--
+
+INSERT INTO `tbl_staff` (`staffID`, `username`, `password`, `status`, `role`) VALUES
+(4, 'Pedro', '1234abcd..', 'Online', 'Mesero'),
+(10, 'Andres', '1234abcd..', 'Online', 'Cajero'),
+(11, 'Pepe', '1234abcd..', 'Online', 'Mesero');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tbl_menu`
+--
+ALTER TABLE `tbl_menu`
+  ADD PRIMARY KEY (`menuID`);
+
+--
+-- Indices de la tabla `tbl_menuitem`
+--
+ALTER TABLE `tbl_menuitem`
+  ADD PRIMARY KEY (`itemID`),
+  ADD KEY `menuID` (`menuID`);
+
+--
+-- Indices de la tabla `tbl_order`
+--
+ALTER TABLE `tbl_order`
+  ADD PRIMARY KEY (`orderID`);
+
+--
+-- Indices de la tabla `tbl_orderdetail`
+--
+ALTER TABLE `tbl_orderdetail`
+  ADD PRIMARY KEY (`orderDetailID`),
+  ADD KEY `itemID` (`itemID`),
+  ADD KEY `orderID` (`orderID`);
+
+--
+-- Indices de la tabla `tbl_staff`
+--
+ALTER TABLE `tbl_staff`
+  ADD PRIMARY KEY (`staffID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_menu`
+--
+ALTER TABLE `tbl_menu`
+  MODIFY `menuID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_menuitem`
+--
+ALTER TABLE `tbl_menuitem`
+  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_orderdetail`
+--
+ALTER TABLE `tbl_orderdetail`
+  MODIFY `orderDetailID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_staff`
+--
+ALTER TABLE `tbl_staff`
+  MODIFY `staffID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
